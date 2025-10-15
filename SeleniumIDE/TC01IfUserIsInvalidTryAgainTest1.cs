9d45ba1f99118a084bb9e9fd320fe83cace444a6
petargeorgiev18 <petar01920192@gmail.com>
2025-10-15 09:33:54 +0300

    Added folder for the yaml file

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -21,6 +21,13 @@ public class TC01IfUserIsInvalidTryAgainTest
     [SetUp]
     public void SetUp()
     {
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("headless");
+        options.AddArgument("no-sandbox");
+        options.AddArgument("disable-dev-shm-usage");
+        options.AddArgument("disable-gpu");
+        options.AddArgument("window-size=1920x1080");
+
         driver = new ChromeDriver();
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
